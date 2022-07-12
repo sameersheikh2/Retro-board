53f0fbf9ae588f7abbb444819e1f75b8477188d0
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-12 21:57:08 +0530

    tried some logics and styling but revert back

--- src/components/Qualities/GoodPointItem.js
+++ src/components/Qualities/GoodPointItem.js
@@ -8,16 +8,18 @@ const GoodPointItem = (props) => {
     setUpvote((prevUpvote) => prevUpvote + 1);
   };
 
-  const removeDataHandler = () =>{
-    props.onRemove(props.id)
-  }
+  const removeDataHandler = () => {
+    props.onRemove(props.id);
+  };
 
   return (
-    <li className="q--goal-item" >
-      {props.children}
-      <button onClick={upvoteHandler}>+{upvote}</button>
-      <button onClick={removeDataHandler}>Delete</button>
-    </li>
+    <>
+      <li className="q--goal-item">
+        <div>{props.children}</div>
+        <button onClick={upvoteHandler}>+{upvote}</button>
+        <button onClick={removeDataHandler}>Delete</button>
+      </li>
+    </>
   );
 };
 
