34c7127a7ba7029e2df2837b49cc9c06e3e8ea07
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-14 14:01:42 +0530

    added state in Input component to get data from input and store it

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React , {useState} from 'react'
 
-const Input = () => {
+const Input = (props) => {
+  const [inputData , setInputData] = useState();
+
+  const onAddHandler = event => {
+    setInputData(event.target.value)
+  }
+
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
+    const data ={
+        id: Math.random(),
+        title: inputData,
+      }
+      props.onGetData(data)
+    }
+
   return (
-    <form  className='form'>
-      <input type="text"  required />
+    <form onSubmit={onSubmitHandler} className='form'>
+      <input type="text"  required onChange={onAddHandler} />
       <button className="form-btn">Add</button>
     </form>
-  )
-}
+  );
+};
 
 export default Input
