e045360a817e6fc43794caf2642b89a66e08a2de
sameersheikh2 <sameersheikh819@gmail.com>
2022-07-14 14:05:11 +0530

    added function to show and hide input in both Improvement and Goodpoint components using state

--- src/components/Qualities/GoodPointsList.js
+++ src/components/Qualities/GoodPointsList.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import Input from '../UI/Input'
-
+import React , {useState} from "react";
+import Input from "../UI/Input";
 
 const GoodPointList = () => {
+  const [showInput, setShowInput] = useState(false);
+
+  const showInputHandler = () => {
+    setShowInput(!showInput);
+  };
   return (
     <>
-    <h2>
-      What went well
-      <button className="btn-red" o>
-        +
-      </button>
-    </h2>
-    <Input/>
-    <ul className="improve--list">
-      
-    </ul>
-  </>
-  )
-}
+      <h2>
+        What went well
+        <button className="btn-red" onClick={showInputHandler}>
+          +
+        </button>
+      </h2>
+      {showInput && <Input />}
+      <ul className="improve--list"></ul>
+    </>
+  );
+};
 
-export default GoodPointList
+export default GoodPointList;
--- src/components/improvement/ImprovementsList.js
+++ src/components/improvement/ImprovementsList.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import Input from '../UI/Input'
+import React, { useState } from "react";
+import Input from "../UI/Input";
 
 const ImprovementList = () => {
+  const [showInput, setShowInput] = useState(false);
+  const showInputHandler = () => {
+    setShowInput(!showInput);
+  };
+
   return (
     <>
       <h2>
         What didn't go well
-        <button className="btn-red" o>
+        <button className="btn-red" onClick={showInputHandler}>
           +
         </button>
       </h2>
-      <Input/>>
-      <ul className="improve--list">
-        
-      </ul>
+      {showInput && <Input />}
+      <ul className="improve--list"></ul>
     </>
-  )
-}
+  );
+};
 
-export default ImprovementList
+export default ImprovementList;
